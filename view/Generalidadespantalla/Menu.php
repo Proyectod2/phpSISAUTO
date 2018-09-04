3b81460475295eaf4115e39dcad9b4f88537961b
brendacg <33129048+brendacg@users.noreply.github.com>
2018-09-04 17:00:43 -0600

    mostrar cliente

--- view/Generalidadespantalla/Menu.php
+++ view/Generalidadespantalla/Menu.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <title>SISAUTO</title>
-        <!-- Bootstrap core CSS-->
-        <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <!-- Custom fonts for this template-->
-        <link href="../assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-        <!-- Custom styles for this template-->
-        <link href="../assets/css/sb-admin.css" rel="stylesheet">
-    </head>
-</head>
-<body class="fixed-nav sticky-footer bg-dark" id="page-top">
-    <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
         <a class="navbar-brand" href="http://localhost/phpSISAUTO/view/index.php">SISAUTO</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -48,10 +23,10 @@ and open the template in the editor.
                     </a>
                     <ul class="sidenav-second-level collapse" id="collapseComponents">
                         <li>
-                            <a href="http://localhost/phpSISAUTO/Compras.php"><span class="fa fa-list"> Compras</span></a>
+                            <a href="http://localhost/phpSISAUTO/view/Compras.php"><span class="fa fa-list"> Compras</span></a>
                         </li>
                         <li>
-                            <a href="http://localhost/phpSISAUTO/Ventas.php"> Ventas</a>
+                            <a href="http://localhost/phpSISAUTO/view/Ventas.php"> Ventas</a>
                         </li>
                     </ul>
                 </li>
@@ -65,7 +40,7 @@ and open the template in the editor.
                             <a href="http://localhost/phpSISAUTO/view/Cliente.php"><span class="fa fa-user"> Cliente</span></a>
                         </li>
                         <li>
-                             <a href="http://localhost/phpSISAUTO/view/Proveedor.php"><span class="fa fa-user"> Proveedor</span></a>
+                            <a href="http://localhost/phpSISAUTO/view/Proveedor.php"><span class="fa fa-user"> Proveedor</span></a>
                         </li>
                         <li>
                             <a href="http://localhost/phpSISAUTO/view/Producto.php"><span class="fa fa-tags"> Producto</span></a>
@@ -189,131 +164,3 @@ and open the template in the editor.
             </ul>
         </div>
     </nav>
-    <div class="content-wrapper" style="background-color:#eff3f4;">
-        <div class="container-fluid">
-            <!-- Breadcrumbs-->
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                    <a href="../view/index.html">Inicio</a>
-                </li>
-                <li class="breadcrumb-item active">Clientes</li>
-            </ol>
-            <div class="row">
-                <div class="col-12">
-                    <a class="pull-right" href="http://localhost/phpSISAUTO/view/AgregarCli.php">
-                        <button class="btn btn-primary" data-toggle="modal" data-target="#modalNuevo">
-                            Agregar nuevo 
-                            <span class="fa fa-plus"></span>
-                        </button>
-                    </a>
-                    <a class="pull-right" href="">
-                        <button class="btn btn-primary" data-toggle="modal" data-target="#modalNuevo">
-                            Reporte
-                            <span class="fa fa-file-pdf-o"></span>
-                        </button>
-                    </a><br><br>
-                    <!-- TABLA CLIENTES-->
-                    <div class="card mb-3">
-                        <div class="card-header">
-                            <i class="fa fa-table"></i>Clientes</div>
-                            <form align="right" ><br>
-                            <table class="pull-right " id="dataTable" width="100%" cellspacing="0" >
-                                <thead>
-                                    <tr>
-                                        <th style="width:200px"></th>
-                                        <th style="width:120px"><div class="input-group" style="width:500px" align="center">
-                                                <input type="text" class="form-control" placeholder="Buscar" name="" align="center">
-                                                <div class="input-group-btn">
-                                                    <button class="btn btn-default" type="submit" title="Buscar"><i class="fa fa-search" ></i></button>
-                                                </div>
-                                            </div></th>
-                                    </tr>
-                                </thead>
-                            </table>
-                        </form>
-                        <div class="card-body">
-                            <div class="table-responsive">
-
-                            <?php include("../confi/Conexion.php"); 
-                            $conexion = conectarMysql();
-                            $sql="SELECT * from cliente order by nombre_Cli ASC";
-                            $clientes= mysqli_query($conexion, $sql) or die("No se puedo ejecutar la consulta"); ?>
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th style="width:175px">Nombre</th>
-                                            <th style="width:175px">Dirección</th>
-                                            <th style="width:85px">Teléfono</th>
-                                            <th style="width:85px">NCR</th>
-                                            <th style="width:85px">NIT</th>
-                                            <th align="center" style="width:2px">Acción</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                    <?php While($cliente=mysqli_fetch_assoc($clientes)){?>
-                                        <tr>
-                                            <td><?php echo $cliente['nombre_Cli'] ?></td>
-                                            <td><?php echo $cliente['direccion_Cli'] ?></td>
-                                            <td><?php echo $cliente['telefono_Cli'] ?></td>
-                                            <td><?php echo $cliente['nrc_Cli'] ?></td>
-                                            <td><?php echo $cliente['nit_Cli'] ?></td>
-                                                                                       
-                                            <th align="center">
-                                                <button title="Editar" type="button" class="btn btn-primary fa fa-pencil-square-o"></button>
-                                                <button title="Eliminar" type="button" class="btn btn-danger fa fa-trash"></button>
-                                            </th>
-                                        </tr>
-                                        <?php } ?>
-                                    </tfoot>
-                                </table>
-                            </div>
-                        </div>
-                        <div class="card-footer small text-muted">Ultima actualización </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- /.container-fluid-->
-    <!-- /.content-wrapper-->
-    <footer class="sticky-footer">
-        <div class="container">
-            <div class="text-center">
-                <small>UES-FMP. DERECHOS RESERVADOS 2018</small>
-            </div>
-        </div>
-    </footer>
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fa fa-angle-up"></i>
-    </a>
-    <!-- Logout Modal-->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">¿Listo para salir?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Seleccione "Cerrar sesión" a continuación si está listo para finalizar su sesión actual.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-                    <a class="btn btn-primary" href="../view/login.html">Cerrar Sesión</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Bootstrap core JavaScript-->
-    <script src="../assets/vendor/jquery/jquery.min.js"></script>
-    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- Core plugin JavaScript-->
-    <script src="../assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-    <!-- Custom scripts for all pages-->
-    <script src="../assets/js/sb-admin.min.js"></script>
-    
-</div>
-</body>
-</html>
-
---
+++ view/Generalidadespantalla/apertura.php
@@ -0,0 +1,18 @@
+   <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>SISAUTO</title>
+        <!-- Bootstrap core CSS-->
+        <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom fonts for this template-->
+        <link href="../assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <!-- Custom styles for this template-->
+        <link href="../assets/css/sb-admin.css" rel="stylesheet">
+        <link href="../assets/pNotify/pnotify.custom.min.css" rel="stylesheet">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
