45bcd8c5ddd793605f3d522d2cda283a1e5bf7c5
brendacg <33129048+brendacg@users.noreply.github.com>
2018-09-06 17:29:48 -0600

    modal ver proveedor

--- view/Proveedor.php
+++ view/Proveedor.php
@@ -79,11 +79,11 @@ and open the template in the editor.
                                             <td><?php echo $proveedore['nombreResp_Prov'] ?></td>
                                                                                        
                                             <th align="center">
-                                                <button type="button" class="btn btn-info fa fa-eye"></button>
+                                                <button type="button" class="btn btn-info fa fa-eye" data-toggle="modal" data-target="#mimodalejemplo"></button>
                                                 <button title="Editar" type="button" class="btn btn-primary fa fa-pencil-square-o"></button>
-                                                <button type="button" class="btn btn-success fa fa-check-square-o "></button>
+                                                <button type="button" class="btn btn-success fa fa-toggle-up "></button>
                                                 <!-- <button type="button" class="btn btn-warning"></button> -->
-                                                <button title="Eliminar" type="button" class="btn btn-danger fa fa-remove"></button>
+                                                <button title="Eliminar" type="button" class="btn btn-danger fa fa-toggle-down"></button>
                                             </th>
                                         </tr>
                                         <?php } ?>
@@ -97,6 +97,80 @@ and open the template in the editor.
             </div>
         </div>
     </div>
+
+<div class="modal fade" id="mimodalejemplo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">Proveedor</h4>
+      </div>
+      <div class="modal-body">
+            <form action="../Controlador/proveedorC.php" method="POST" align="center" autocomplete="off">
+                        <h5 align="center">Datos Generales</h5><br>
+                        <div class="form-group row">
+                            <label for="empresa" class="col-sm-12 col-md-2 col-form-label">Nombre de la Empresa:</label>
+                            <div class="col-sm-12 col-md-10">
+                                <input class="form-control" placeholder="Nombre Completo" type="text" id="empresa" name="Nombre_Emp" style="width:600px;height:40px">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="empresa" class="col-sm-12 col-md-2 col-form-label">Correo:</label>
+                            <div class="col-sm-12 col-md-10">
+                                <input class="form-control" placeholder="Correo empresa" type="text" id="empresa" name="Correo_Emp" style="width:600px;height:40px">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label  for="tele1" class="col-sm-12 col-md-2 col-form-label">Teléfono:</label>
+                            <div  class="col-sm-12 col-md-10">
+                                <input class="form-control" type="tel" id="tele1" placeholder="XXXX-XXXX" name="Telefono_Emp" style="width:110px;height:40px">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="direccion" class="col-sm-12 col-md-2 col-form-label">Dirección:</label>
+                            <div class="col-sm-12 col-md-10">
+                                <input class="form-control" type="text" placeholder="Dirección" name="Direccion_Emp" style="width:600px;height:40px" id="direccion">
+                            </div>
+                        </div><br>
+                        <hr size="50" style="background-color: #78bab9"/><br>
+                        <h5 align="center">Datos del Responsable</h5><br>
+                        <div class="form-group row">
+                            <label for="responsable" class="col-sm-12 col-md-2 col-form-label">Nombre del Responsable:</label>
+                            <div class="col-sm-12 col-md-10">
+                                <input class="form-control" type="text" placeholder="Nombre Completo" name="Nombre_Res" style="width:600px;height:40px" id="responsable">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="tel2" class="col-sm-12 col-md-2 col-form-label">Teléfono:</label>
+                            <div class="col-sm-12 col-md-10">
+                                <input class="form-control" type="tel" data-mask="9999-9999" placeholder="XXXX-XXXX" name="Telefono_Res" style="width:110px;height:40px" id="tel2">
+                            </div>
+                        </div><br>
+                        <hr width="75%" />
+                    </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+       
+      </div>
+    </div>
+  </div>
+</div>
+
+    <!-- <div class="modal fade" id="modalverprov" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myModalLabel">Proveedor</h4>
+                </div>
+                <div class="modal-body">
+                    
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div> -->
 
         <?php include("Generalidadespantalla/cierre.php"); ?>
 </body>
